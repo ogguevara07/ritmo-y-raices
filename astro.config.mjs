5c87c52f3e4736c0d4f189ff72c9b601eeb471de
Orlando Guevara <guevara4536@hotmail.com>
2025-11-29 11:09:32 -0500

    Configures base URL for deployment
    Sets the `site` and `base` properties in the Astro configuration file.
    
    This ensures that the application is served correctly from the specified
    base URL, which is required for deployment to GitHub Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://ogguevara07.github.io/',
+    base: '/ritmo-y-raices',
   vite: {
     plugins: [tailwindcss()]
   }
